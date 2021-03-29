0656925345d2f0f1e0de35a6cf2d7421ccae4e35
Thomas Reznik <thomasreznik2000@gmail.com>
2021-03-29 15:36:57 -0700

    comment

--- app/src/main/java/com/example/android_team_project/MainActivity.java
+++ app/src/main/java/com/example/android_team_project/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("TEST");
+        //Thomas Comment for new branch
     }
 }
