3ba8c255de581a081eef47b32d6c1edd46e77000
Thomas Reznik <thomasreznik2000@gmail.com>
2021-04-06 20:01:48 -0700

    Added Data for Beauty and Entertainment (except for images)

--- app/src/main/java/com/example/android_team_project/MainActivity.java
+++ app/src/main/java/com/example/android_team_project/MainActivity.java
@@ -65,16 +65,52 @@ public class MainActivity extends AppCompatActivity {
 
 
                                         //************************ BEAUTY CATEGORY***********************
-        allLocations.add(new Beauty("Sports Clips", "Hair salon in Redlands", "1" ,"$", "nice place" , ""));
-        allBeauty.put("Sports Clips", new Beauty("Sports Clips", "Hair salon in Redlands", "1", "$", "nice place", ""));
+
+        //Sports Clips
+        allLocations.add(new Beauty("Sports Clips Haircuts of Redlands", "Hair Salon that includes sports on TV, steamed towel treatment, and a fantastic haircut", "34.07493746667798, -117.20601048019489" ,"$", "Chill Place to watch the game while getting the fresh cut. Feeling snazzier than ever" , ""));
+        allBeauty.put("Sports Clips Haircuts of Redlands", new Beauty("Sports Clips Haircuts of Redlands", "Hair Salon that includes sports on TV, steamed towel treatment, and a fantastic haircut", "34.07493746667798, -117.20601048019489", "$", "Chill Place to watch the game while getting the fresh cut. Feeling snazzier than ever", ""));
+
+        //Revive Salon & Barber
+        allLocations.add(new Beauty("Revive Salon & Barber", "Salon that offers a variety of services such as salon services, barbershop, and skin care", "34.064658625049844, -117.22587126447404" ,"$$", "Good quality cuts with upfront pricing on all their work" , ""));
+        allBeauty.put("Revive Salon & Barber", new Beauty("Revive Salon & Barber", "Salon that offers a variety of services such as salon services, barbershop, and skin care", "34.064658625049844, -117.22587126447404", "$$", "Good quality cuts with upfront pricing on all their work", ""));
+
+        //Hairajuku
+        allLocations.add(new Beauty("Hairajuku", "Hair salon in Redlands, CA.", "34.06075165419431, -117.18622063124148" ,"$$", "Kat is amazing! If you have dark hair with Stubborn gray or you want a fun color and think you never can with your hair color being dark.  This girl can perform miracles." , ""));
+        allBeauty.put("Hairajuku", new Beauty("Hairajuku", "Hair salon in Redlands, CA.", "34.06075165419431, -117.18622063124148", "$$", "Kat is amazing! If you have dark hair with Stubborn gray or you want a fun color and think you never can with your hair color being dark.  This girl can perform miracles.", ""));
+
+        //Ancient Thai Massage
+        allLocations.add(new Beauty("Ancient Thai Massage", "Massage spa in Redlands, CA", "34.065867471822195, -117.18276867134257" ,"$$", "Best massage I’ve ever had. Very professional and relaxing. I’ve been here about 4 times and was never disappointed. Highly recommend" , ""));
+        allBeauty.put("Ancient Thai Massage", new Beauty("Ancient Thai Massage", "Massage spa in Redlands, CA", "34.065867471822195, -117.18276867134257", "$$", "Best massage I’ve ever had. Very professional and relaxing. I’ve been here about 4 times and was never disappointed. Highly recommend", ""));
+
+        //European Wax Center
+        allLocations.add(new Beauty("European Wax Center", "Waxing center in Redlands, CA.", "34.07335228209944, -117.21156026827077" ,"$$", "Friendly staff, great service, easy parking." , ""));
+        allBeauty.put("European Wax Center", new Beauty("European Wax Center", "Waxing center in Redlands, CA.", "34.07335228209944, -117.21156026827077", "$$", "Friendly staff, great service, easy parking.", ""));
+
 
 
                                         //************************ ENTERTAINMENT CATEGORY***********************
-        allLocations.add(new Entertainment("Mountain Grove Shopping Center", "mall in Redlands", "" ,"$$$", "convinient place" , ""));
-        allEntertainment.put("Mountain Grove Shopping Center", new Entertainment("Mountain Grove Shopping Center", "mall in Redlands", "", "$$$", "convinient place" , ""));
+       //Harkins Theatres
+        allLocations.add(new Entertainment("Harkins Theatres", "Modern multiplex chain showing blockbusters & a few independent films; some venues offer childcare.", "34.074507097782416, -117.20591811837417" ,"$$", "Powered recliners included with regular tickets, comfy seats, great atmosphere" , ""));
+        allEntertainment.put("Harkins Theatres", new Entertainment("Harkins Theatres", "Modern multiplex chain showing blockbusters & a few independent films; some venues offer childcare.", "34.074507097782416, -117.20591811837417", "$$", "Powered recliners included with regular tickets, comfy seats, great atmosphere" , ""));
+
+        //Studio Movie Grill
+        allLocations.add(new Entertainment("Studio Movie Grill", "First-run, classic & kids' films, plus a varied American menu & cocktails brought to guests' seats", "34.060287026371284, -117.18480377072386" ,"$$", "Love the service they provide they bring your food, drinks to your seat." , ""));
+        allEntertainment.put("Studio Movie Grill", new Entertainment("Studio Movie Grill", "First-run, classic & kids' films, plus a varied American menu & cocktails brought to guests' seats", "34.060287026371284, -117.18480377072386", "$$", "Love the service they provide they bring your food, drinks to your seat." , ""));
+
+        //Redlands Footlighters Theatre
+        allLocations.add(new Entertainment("Redlands Footlighters Theatre", "Intimate, long-running venue showcasing comedies, classics & other community theater performances.", "34.05189606491037, -117.21690444560686" ,"$", "Small town feel with great actors. Tickets are absolutely affordable and you will be entertained as the seats are so close to the stage. An intimate experience" , ""));
+        allEntertainment.put("Redlands Footlighters Theatre", new Entertainment("Redlands Footlighters Theatre", "Intimate, long-running venue showcasing comedies, classics & other community theater performances.", "34.05189606491037, -117.21690444560686", "$", "Small town feel with great actors. Tickets are absolutely affordable and you will be entertained as the seats are so close to the stage. An intimate experience" , ""));
+
+        //Empire Bowl
+        allLocations.add(new Entertainment("Empire Bowl", "Popular long-standing venue featuring 24 bowling lanes, arcade games, a snack bar & cocktail lounge.", "34.065186434031254, -117.1944633709442" ,"$", "A great place for family and friends to relax and bowl. The couches are a nice added touch. The bowling alley is well-maintained and reasonably priced." , ""));
+        allEntertainment.put("Empire Bowl", new Entertainment("Empire Bowl", "Popular long-standing venue featuring 24 bowling lanes, arcade games, a snack bar & cocktail lounge.", "34.065186434031254, -117.1944633709442", "$", "A great place for family and friends to relax and bowl. The couches are a nice added touch. The bowling alley is well-maintained and reasonably priced." , ""));
+
+        //Vector Escape Games
+        allLocations.add(new Entertainment("Vector Escape Games", "Vector Escape Games is here to bring the fun. With energetic staff and actors we strive to bring the next generation of escape rooms.", "34.067477293754244, -117.2150455616178" ,"$$", "There are 3 rooms to choose from and they have amazing deals and rooms!" , ""));
+        allEntertainment.put("Vector Escape Games", new Entertainment("Vector Escape Games", "Vector Escape Games is here to bring the fun. With energetic staff and actors we strive to bring the next generation of escape rooms.", "34.067477293754244, -117.2150455616178", "$$", "There are 3 rooms to choose from and they have amazing deals and rooms!" , ""));
 
 
-                                        //************************ FOOD CATEGORY***********************
+        //************************ FOOD CATEGORY***********************
         allLocations.add(new Food("El Burrito", "burrito place", "" ,"$", "super good food" ,""));
         allFood.put("El Burrito", new Food("El Burrito", "burrito place", "", "$", "super good food", ""));
 
