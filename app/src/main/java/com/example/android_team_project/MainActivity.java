0d0e9f6e7f75281c167db643591b684e820f4ae2
Andrew Yip <andrew.yip22@gmail.com>
2021-04-07 23:10:37 -1000

    created references to UI elements (category buttons)

--- app/src/main/java/com/example/android_team_project/MainActivity.java
+++ app/src/main/java/com/example/android_team_project/MainActivity.java
@@ -5,14 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    // UI REFERENCES
+    ImageButton barsBtn;
+    ImageButton beautyBtn;
+    ImageButton entertainmentBtn;
+    ImageButton foodBtn;
+    ImageButton fitnessBtn;
+    ImageButton hotelBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_page);
-        System.out.println("TEST");
-        //Thomas Comment for new branch
+
+        barsBtn = (ImageButton) findViewById(R.id.barsBtn);
+        beautyBtn = (ImageButton) findViewById(R.id.beautyBtn);
+        entertainmentBtn = (ImageButton) findViewById(R.id.entertainmentBtn);
+        foodBtn = (ImageButton) findViewById(R.id.foodBtn);
+        fitnessBtn = (ImageButton) findViewById(R.id.fitnessBtn);
+        hotelBtn = (ImageButton) findViewById(R.id.hotelBtn);
+
     }
 }
